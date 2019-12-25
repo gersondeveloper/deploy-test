5b4000257cba1e5819e2b4015a444bee9bc40b48
Gerson C Filho <gersoncfilho@mac.com>
2019-12-25 03:40:47 -0300

    Updates

---
+++ precache-manifest.ab324744fc0aecc3f286d59eb26233ce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc633388d2f7a7b20657ba033d3c0092",
+    "url": "/deploy-test/index.html"
+  },
+  {
+    "revision": "30f93a87261f4e91c1ed",
+    "url": "/deploy-test/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "ab5480a2ae474e641a01",
+    "url": "/deploy-test/static/js/2.0f64dbeb.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/deploy-test/static/js/2.0f64dbeb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "30f93a87261f4e91c1ed",
+    "url": "/deploy-test/static/js/main.34de6dd7.chunk.js"
+  },
+  {
+    "revision": "d532cb1c657a81d60475",
+    "url": "/deploy-test/static/js/runtime-main.74206ad9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/deploy-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
